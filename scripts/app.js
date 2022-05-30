26b3d66e57fb1bd8ca22ae291eb042eb4df38d2f
rakinnir <98404687+rakinnir@users.noreply.github.com>
2022-05-30 15:58:13 +0600

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+// declaring variables
+const form = document.querySelector(".change-location")
+const outputInfo = document.querySelector(".details")
+const card = document.querySelector(".card")
+const time = document.querySelector(".time")
+const icon = document.querySelector(".icon img")
+
+const updateUi = (data) => {
+  const { cityDetails, weather } = data
+
+  const info = `
+          <h5 class="my-3">${cityDetails.EnglishName}</h5>
+          <div class="my-3">${weather.WeatherText}</div>
+          <div class="display-4 my-4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;C</span>
+          </div>
+          `
+  outputInfo.innerHTML = info
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none")
+  }
+
+  // ternali condition for checking time
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg"
+  time.setAttribute("src", timeSrc)
+
+  // icons corresponding to weather
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute("src", iconSrc)
+}
+
+// starting function
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city)
+  const weather = await getWeather(cityDetails.Key)
+  return { cityDetails, weather }
+}
+
+// input field function
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const city = form.city.value.trim()
+  form.reset()
+
+  updateCity(city)
+    .then((data) => {
+      console.log(data)
+      updateUi(data)
+    })
+    .catch((err) => {
+      console.log(err, "can't fetch data")
+    })
+})
